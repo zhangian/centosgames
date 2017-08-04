5a7fbaecb3eae6471bd3beadef5d207361f8c157
zhangmr <zhangmr@zendaimoney.com>
2017-08-04 09:48:52 +0800

    init app

---
+++ _nuxt/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="__nuxt">
+    <nuxt-loading ref="loading"></nuxt-loading>
+    <component v-if="layout" :is="layout"></component>
+  </div>
+</template>
+
+<script>
+import NuxtLoading from './components/nuxt-loading.vue'
+
+import '~assets/css/reset.css'
+
+
+let layouts = {
+
+  "_default": () => import('E:\\4_studyDemo\\nuxtJs\\games\\layouts\\default.vue'  /* webpackChunkName: "layouts/default" */)
+
+}
+
+export default {
+  head: {"title":"games","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1"},{"hid":"description","name":"description","content":"gass a games"}],"link":[{"rel":"icon","type":"image/x-icon","href":"/favicon.ico"}],"style":[],"script":[]},
+  data: () => ({
+    layout: null,
+    layoutName: ''
+  }),
+  
+  mounted () {
+    this.$loading = this.$refs.loading
+    this.$nuxt.$loading = this.$loading
+  },
+  
+  methods: {
+    setLayout (layout) {
+      if (!layout || !layouts['_' + layout]) layout = 'default'
+      this.layoutName = layout
+      let _layout = '_' + layout
+      this.layout = layouts[_layout]
+      return this.layout
+    },
+    loadLayout (layout) {
+      if (!layout || !layouts['_' + layout]) layout = 'default'
+      let _layout = '_' + layout
+      if (typeof layouts[_layout] !== 'function') {
+        return Promise.resolve(layouts[_layout])
+      }
+      return layouts[_layout]()
+      .then((Component) => {
+        layouts[_layout] = Component
+        return layouts[_layout]
+      })
+      .catch((e) => {
+        if (this.$nuxt) {
+          return this.$nuxt.error({ statusCode: 500, message: e.message })
+        }
+        console.error(e)
+      })
+    }
+  },
+  components: {
+    NuxtLoading
+  }
+}
+</script>
+
